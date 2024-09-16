0447f71d00659e1a82527b93508f6297af96363b
Adam A <adam@hack.bg>
2024-09-16 18:32:46 +0300

    add TextLineStream for @std/streams dependency and manually write to stdout

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM ghcr.io/anoma/namada:v0.43.0 AS namada
 
+# Install system dependencies
 USER root
 RUN apt update \
  && apt install -y vim curl wget unzip simpleproxy
@@ -9,8 +10,8 @@ RUN cd /tmp \
  && mv deno /usr/local/bin \
  && rm deno-x86_64-unknown-linux-gnu.zip
 
+# Configure node
 USER namada
-
 ENV NAMADA_NETWORK_CONFIGS_SERVER="https://testnet.knowable.run/configs"
 ENV CHAIN_ID="housefire-head.a03c8e8948ed20b"
 ENV DATA_DIR=/home/namada/.local/share/namada/$CHAIN_ID
@@ -18,6 +19,10 @@ ENV WASM_DIR=$DATA_DIR/wasm
 ENV CONFIG_DIR=$DATA_DIR/config.toml
 RUN namadac utils join-network --chain-id $CHAIN_ID --wasm-dir $WASM_DIR
 RUN sed -i 's#persistent_peers = ".*"#persistent_peers = "tcp://d6691dc866be3de0be931d2018e8fdc6a564de20@localhost:26666"#' $CONFIG_DIR
+
+# Install control script
+ADD deno.json /deno.json
+RUN deno cache
 ADD control.js /control.js 
 ENTRYPOINT [ "/bin/bash" ]
 CMD [ "-c", "/control.js" ]
--- control.js
+++ control.js
@@ -1,25 +1,98 @@
 #!/usr/bin/env -S deno run --allow-net --allow-run=namada,simpleproxy --allow-env=LOCAL,REMOTE,CONTROL_HOST,CONTROL_PORT
 
-Deno.addSignalListener("SIGINT", () => {
-  console.log("Sayonara!")
-  Deno.exit()
-})
+import { TextLineStream } from "@std/streams"
 
-const LOCAL        = Deno.env.get("LOCAL")        || ":26666"
-const REMOTE       = Deno.env.get("REMOTE")       || "165.227.42.204:26656"
-const CONTROL_HOST = Deno.env.get("CONTROL_HOST") || "127.0.0.1"
-const CONTROL_PORT = Deno.env.get("CONTROL_PORT") || "25555"
+function main () {
+  // Exit cleanly on Ctrl-C (otherwise container just detaches)
+  Deno.addSignalListener("SIGINT", () => Deno.exit())
+
+  // Read configuration from environment.
+  const LOCAL        = Deno.env.get("LOCAL")        || ":26666"
+  const REMOTE       = Deno.env.get("REMOTE")       || "165.227.42.204:26656"
+  const CONTROL_HOST = Deno.env.get("CONTROL_HOST") || "127.0.0.1"
+  const CONTROL_PORT = Deno.env.get("CONTROL_PORT") || "25555"
+
+  // Define services.
+  const services = {
+    node:  new Service('Namada fullnode', 'namada', 'node', 'ledger', 'run'),
+    proxy: new Service('TCP proxy', 'simpleproxy', 'v', '-L', LOCAL, '-R', REMOTE)
+  }
+
+  // Define routes.
+  // TODO: define full route map here and not just list of route names
+  const routes = []
+  for (const service of Object.keys(services)) {
+    routes.push(`/${service}/start`)
+    routes.push(`/${service}/stop`)
+  }
+
+  // Run service
+  Deno.serve({ host: CONTROL_HOST, port: CONTROL_PORT }, (req) => {
+    try {
+      // Service status
+      const info = {
+        config:   { LOCAL, REMOTE },
+        services: { proxy: services.proxy.status, node: services.node.status },
+        commands: routes
+      }
+
+      // Trim trailing slashes
+      let { pathname } = new URL(req.url)
+      while (pathname.endsWith('/')) pathname = pathname.slice(0, pathname.length - 1)
+
+      // Route requests
+      switch (pathname) {
+        case '/node/start':
+          services.node.start()
+          return redirect('/')
+        case '/node/stop':
+          services.node.stop()
+          return redirect('/')
+        case '/proxy/start':
+          services.proxy.start()
+          return redirect('/')
+        case '/proxy/stop':
+          services.proxy.stop()
+          return redirect('/')
+        case '':
+          return respond(200, info)
+        default:
+          return respond(404, { error: 'not found' })
+      }
+
+    } catch (e) {
+      console.error(e)
+      return respond(500, { error: e.message||e })
+    }
+
+    function redirect (pathname) {
+      const url = new URL(req.url)
+      url.pathname = pathname
+      return Response.redirect(url)
+    }
+
+    function respond (status, data) {
+      const headers = { "content-type": "application/json" }
+      return new Response(JSON.stringify(data, null, 2), { status, headers })
+    }
+
+  })
+}
 
 class Service {
+
   constructor (name, command, ...args) {
     this.name    = name
     this.command = command
     this.args    = args
   }
+
   process = null
+
   get status () {
     return !!this.process
   }
+
   start () {
     console.log('Starting:', this.name)
     if (this.process) {
@@ -29,10 +102,24 @@ class Service {
     const options = { args: this.args, stdout: 'piped', stderr: 'piped' }
     this.process = new Deno.Command(this.command, options).spawn()
     console.log('Started:', this.name, 'at PID:', this.process.pid)
-    this.process.stdout.pipeTo(Deno.stdout.writable, { preventClose: true })
-    this.process.stderr.pipeTo(Deno.stderr.writable, { preventClose: true })
+
+    //const transform = (chunk, controller) => { controller.enqueue(`${this.name}: ${chunk}`) }
+
+    this.process.stdout
+      .pipeThrough(new TextLineStream())
+      .pipeTo(new WritableStream({ write (chunk, _) {
+        console.log(`[${name}] [stderr]: ${chunk}`)
+      } }))
+
+    this.process.stderr
+      .pipeThrough(new TextLineStream())
+      .pipeTo(new WritableStream({ write (chunk, _) {
+        console.log(`[${name}] [stderr]: ${chunk}`)
+      } }))
+
     return true
   }
+
   stop () {
     console.log('Stopping:', this.name)
     if (!this.process) {
@@ -45,57 +132,7 @@ class Service {
     console.log('Stopped:', this.name, 'at PID:', pid)
     return true
   }
+
 }
 
-const services = {
-  node:  new Service('Namada fullnode', 'namada', 'node', 'ledger', 'run'),
-  proxy: new Service('TCP proxy', 'simpleproxy', "-L", LOCAL, "-R", REMOTE)
-}
-const routes = []
-for (const service of Object.keys(services)) {
-  routes.push(`/${service}/start`)
-  routes.push(`/${service}/stop`)
-}
-
-Deno.serve({ host: CONTROL_HOST, port: CONTROL_PORT }, (req) => {
-  try {
-    const info = {
-      config:   { LOCAL, REMOTE },
-      services: { proxy: services.proxy.status, node: services.node.status },
-      commands: routes
-    }
-    let { pathname } = new URL(req.url)
-    while (pathname.endsWith('/')) pathname = pathname.slice(0, pathname.length - 1)
-    switch (pathname) {
-      case '/node/start':
-        services.node.start()
-        return redirect('/')
-      case '/node/stop':
-        services.node.stop()
-        return redirect('/')
-      case '/proxy/start':
-        services.proxy.start()
-        return redirect('/')
-      case '/proxy/stop':
-        services.proxy.stop()
-        return redirect('/')
-      case '':
-        return respond(200, info)
-      default:
-        return respond(404, { error: 'not found' })
-    }
-  } catch (e) {
-    console.error(e)
-    return respond(500, { error: e.message||e })
-  }
-
-  function redirect (pathname) {
-    const url = new URL(req.url)
-    url.pathname = pathname
-    return Response.redirect(url)
-  }
-  function respond (status, data) {
-    const headers = { "content-type": "application/json" }
-    return new Response(JSON.stringify(data, null, 2), { status, headers })
-  }
-})
+if (import.meta.main) main()
